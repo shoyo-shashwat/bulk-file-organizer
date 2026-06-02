6502b2c4eb5d706102deff19178a191041b20528
shoyo-shashwat <shashwatshukla2504@gmail.com>
2026-06-02 20:31:51 +0530

    Add FILE_TYPE_NAME dictionary with file extension mappings

--- organiser.py
+++ organiser.py
@@ -6,6 +6,23 @@ import argparse
 # This makes path manipulation more intuitive and cross-platform compatible.
 import pathlib
 
+#(BRAINS OF OUR FILE. KEY- folder we create eg-Images and Value- file extension(.png,.jpg))
+FILE_TYPE_NAME={
+      
+   "Images" : ['.jpeg','.jpg','.gif','.svg','.png'],
+    "Documents" : ['.txt','.pdf','.doc','.ppt','.xls','.xlsx'],
+    "Audio" : ['.mp3','.wav','.aac','.flac'],
+    "Videos" : ['.mp4','.avi','.mkv','.mov'],
+    "Archives" : ['.zip','.rar','.tar','.gz'],
+    "Others" : []               # This category will be used for files that don't match any of the above types
+    
+    
+    
+    
+    
+    
+    }  
+
 # This block of code will only run when the script is executed directly
 # from the command line. It's the main entry point for our application.
 
@@ -17,3 +34,4 @@ if __name__ == "__main__":
     args = parser.parse_args()                                        #(This object contains the user-provided arguments as attributes.)
     
     print(f"organising files in: {args.source_directory}")
+    
