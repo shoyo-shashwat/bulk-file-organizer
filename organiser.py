58fa1a577fd1c81a3fee0571fcd19edb0c503e1e
shoyo-shashwat <shashwatshukla2504@gmail.com>
2026-06-02 17:45:55 +0530

    Add argparse setup with source_directory argument

--- organiser.py
+++ organiser.py
@@ -5,3 +5,15 @@ import argparse
 # Import the pathlib module to work with file system paths in an object-oriented way.
 # This makes path manipulation more intuitive and cross-platform compatible.
 import pathlib
+
+# This block of code will only run when the script is executed directly
+# from the command line. It's the main entry point for our application.
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(description="Organise files in a directory by their type")
+    
+    parser.add_argument('source_directory', help='The path to the directory you want to organize.')    #(added a postional argument)
+    
+    args = parser.parse_args()                                        #(This object contains the user-provided arguments as attributes.)
+    
+    print(f"organising files in: {args.source_directory}")
